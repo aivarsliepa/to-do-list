ac717e4b784c4cadd35b3e0a62d374ddc3be2711
Aivars Liepa <aiciz@inbox.lv>
2017-11-19 17:31:43 +0200

    removing e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,11 @@
 import { AppPage } from './app.po';
 
 describe('to-do-list App', () => {
-  let page: AppPage;
 
   beforeEach(() => {
-    page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('this is just a stub, currently no e2e tests', () => {
+    expect(true).toBeTruthy();
   });
 });
