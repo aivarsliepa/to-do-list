ac74132358f4051cd1d18b7301cd505cf2b8d2da
Aivars Liepa <aiciz@inbox.lv>
2017-11-25 18:22:05 +0200

    app component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,17 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { ListComponent } from './components/list/list.component';
 import { AuthService } from './services/auth.service';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ItemFormComponent } from './components/item-form/item-form.component';
+import { authServiceMock, getUserSubject, mockUser, listServiceMock } from './testing/mocks';
+import { ListService } from './services/list.service';
 
 
 describe('AppComponent', () => {
+    let fixture: ComponentFixture<AppComponent>;
+    let app;
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [
@@ -21,13 +25,24 @@ describe('AppComponent', () => {
                 ItemFormComponent
             ],
             providers: [
-                { provide: AuthService, useValue: {} }
+                { provide: AuthService, useValue: authServiceMock },
+                { provide: ListService, useValue: listServiceMock }
             ]
         }).compileComponents();
     }));
-    it('should create the app', async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
-        expect(app).toBeTruthy();
+    beforeEach(async(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        app = fixture.debugElement.componentInstance;
+        fixture.detectChanges();
     }));
+
+    it('should create the app', () => {
+        expect(app).toBeTruthy();
+    });
+
+    it('should create the app with logged in user', () => {
+        getUserSubject.next(mockUser);
+        fixture.detectChanges();
+        expect(app).toBeTruthy();
+    });
 });
